e0f0fa3a88fbe81c102d4042ddcb54ca4e324c6c
AndyFooGuoZhen <ndfucoding@gmail.com>
2023-11-12 15:41:56 -0600

    Cleanup

--- wsDemo 8.48.17 AM/src/test/java/com/example/wsServer/wsDemo/WsDemoApplicationTests.java
+++ wsDemo 8.48.17 AM/src/test/java/com/example/wsServer/wsDemo/WsDemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.involveininnovation.chat;
+package com.example.wsServer.wsDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatApplicationTests {
+class WsDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
